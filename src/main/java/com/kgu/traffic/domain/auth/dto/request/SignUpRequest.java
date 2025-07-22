0e2c023d97bd46ffe44d45767911031331021b9d
gituserchan <pcj4142@naver.com>
2025-07-22 09:48:47 +0900

    Feat: 회원가입 및 로그인 Dto 구현 #2

---
+++ src/main/java/com/kgu/traffic/domain/auth/dto/request/SignUpRequest.java
@@ -0,0 +1,8 @@
+package com.kgu.traffic.domain.auth.dto.request;
+
+public record SignUpRequest(
+        String loginId,
+        String password,
+        String name,
+        String region
+) {}
